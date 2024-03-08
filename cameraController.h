a113a63d376f26cdb528e054167f150110c05e78
projetowarmup <projetowarmup@edra.myguest.virtualbox.org>
2024-03-08 14:55:31 -0300

    :sparkles: feat: Adicionado distancia do QR Code e a camera

--- cameraController.h
+++ cameraController.h
@@ -1,10 +1,14 @@
 #include <opencv2/opencv.hpp>
 #include <zbar.h>
+#include <math.h>
 
 class Camera {
 public:
     bool is_scanning_QR_code = false;
     int camera_index = 0;
+    float known_qr_code_size = 10; // Tamanho real do QR code em cm
+    float focal_length = 500.0; // Distância focal da câmera em pixels (valor pré-definido)
+
 
 private:
     cv::VideoCapture capture;
@@ -43,14 +47,18 @@ public:
                         qr_points.emplace_back(symbol->get_location_x(i), symbol->get_location_y(i));
                     }
 
+                    float qr_area = cv::contourArea(qr_points);
+                    float distance = (known_qr_code_size * focal_length) / sqrt(qr_area);
+
+                    std::cout << "Distância: " << distance << " cm" << std::endl;
                     cv::polylines(frame, qr_points, true, cv::Scalar(0, 255, 0), 2);
                 }
             }
+        }
 
             cv::imshow("Camera", frame);
             cv::waitKey(30);
         }
-    }
 
     void set_scanning_QR_code(bool is_scanning) {
         this->is_scanning_QR_code = is_scanning;
@@ -60,5 +68,12 @@ public:
         this->camera_index = index;
         capture.release();
         capture.open(camera_index);
+    }
+
+    void set_focal_length(int size) {
+        this->focal_length = size;
+    }
+    void set_qr_code_size(int size) {
+        this->known_qr_code_size = size;
     }
 };
--- main.cpp
+++ main.cpp
@@ -7,14 +7,19 @@ int main() {
 
     // Define se é para escanear QRCodes
     camera.set_scanning_QR_code(true);
-
     // Define qual camera usar
     camera.set_camera_index(0);
+    //Distância focal da câmera em pixels 
+    camera.set_focal_length(500.0);
+    //Tamanho do QR Code em cm
+    camera.set_qr_code_size(10.0);
 
+    std::cout << "Code ON!" << std::endl;
     while (true) {
         camera.show_image();
         // Verifica se o usuário pressionou a tecla 'q' para sair do loop
         if (cv::waitKey(1) == 'q') {
+            std::cout << "Code OFF!" << std::endl;
             break;
         }
     }
